f3c84d81219f8c98f5edb5d9dd6f13a851169fb2
Oydual3 <claucondor@gmail.com>
2023-12-18 23:53:23 -0400

    feat: add generic button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { FOLLOW_BUTTON } from "./assets/buttons";
 import { Button } from "./components/button";
 
 export function App() {
   return (
-    <div>
-      <h1>React App</h1>
-      <Button text="Boton 1" />
-    </div>
+    <article>
+      <header>
+        <img
+          src="https://pbs.twimg.com/profile_images/1683235367/28_400x400.jpg"
+          alt="Imagen de usuario"
+        />
+        <div>
+          <strong>Nombre de usuario</strong>
+          <span>@usuario</span>
+        </div>
+      </header>
+
+      <aside>
+        <Button text="Boton 1" icon={FOLLOW_BUTTON} />
+      </aside>
+    </article>
   );
 }
