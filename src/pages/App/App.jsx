5dde97cf48109f36830f4bc1455f048b0789c7c2
Oydual3 <claucondor@gmail.com>
2023-12-25 19:07:04 -0400

    Add flexbox layout to App component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,5 +2,23 @@ import { TwitterFollowCard } from "../../components/twitter-follow-card";
 import "./App.css";
 
 export function App() {
-  return <TwitterFollowCard userName="elcondor99" name="Claudio Condor" />;
+  return (
+    <section className="App">
+      <TwitterFollowCard
+        userName="elcondor99"
+        name="Claudio Condor"
+        isFollowing={true}
+      />
+      <TwitterFollowCard
+        userName="dixonortizch"
+        name="Dixon Alberto Ortiz Chandia"
+        isFollowing={false}
+      />
+      <TwitterFollowCard
+        userName="ocandocrypto"
+        name="Angela"
+        isFollowing={true}
+      />
+    </section>
+  );
 }
